8bd3208db000056768c22d510e8b7700df83682c
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-04-05 14:37:41 +0800

    Updated level1

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public int stage;
+
+    static string[] levelDifficulties = { "easy", "medium", "hard" };
+
+    string currentLevelDifficulty = levelDifficulties[0];
+
+
+    public void SetStage(int stageNumber)
+    {
+        PlayerPrefs.SetInt("stageNumber", stageNumber);
+        this.stage = stageNumber;
+    }
+
+    public int GetStageFromPlayerPrefs()
+    {
+        return PlayerPrefs.GetInt("stageNumber");
+    }
+
+    public void SetDifficulty(int difficultyLevel)
+    {
+        this.currentLevelDifficulty = levelDifficulties[difficultyLevel];
+    }
+
+    public string GetDifficulty()
+    {
+        return this.currentLevelDifficulty;
+    }
+
+    public int GetStage()
+    {
+        return stage;
+    }
+
+}
