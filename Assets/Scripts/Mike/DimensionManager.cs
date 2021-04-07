74ba988c11e325a936aefa487d710c2071aa4074
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-04-07 15:30:12 +0800

    Updated line

--- Assets/Scripts/Mike/DimensionManager.cs
+++ Assets/Scripts/Mike/DimensionManager.cs
@@ -12,21 +12,27 @@ public class DimensionManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // float YConstant = 0.75f;
+        float XDistanceFromGround = 0.75f;
         dimensionText.text = dimensionLength.ToString();
-        dimensionText.transform.position= new Vector3(dimensionLength/2, 0,0);
-        position = new Vector3[6];
-        position[0] = new Vector3(0, 0.75f, 0);
-        position[1] = new Vector3(0, -0.75f, 0);
-        position[2] = new Vector3(0,0f,0);
-        position[3] = new Vector3(dimensionLength, 0, 0);
-        position[4] = new Vector3(dimensionLength, 0.75f, 0);
-        position[5] = new Vector3(dimensionLength, -0.75f, 0); 
+        dimensionText.transform.position = new Vector3(dimensionLength / 2, XDistanceFromGround, 0);
+        // position = new Vector3[6];
+        // position[0] = new Vector3(0, YConstant, 0);
+        // position[1] = new Vector3(0, -YConstant, 0);
+        // position[2] = new Vector3(0, XDistanceFromGround, 0);
+        // position[3] = new Vector3(dimensionLength, XDistanceFromGround, 0);
+        // position[4] = new Vector3(dimensionLength, YConstant, 0);
+        // position[5] = new Vector3(dimensionLength, -YConstant, 0);
+        position = new Vector3[2];
+        position[0] = new Vector3(0, XDistanceFromGround, 0);
+        position[1] = new Vector3(dimensionLength, XDistanceFromGround, 0);
+
         dimensionLine.positionCount = position.Length;
         dimensionLine.SetPositions(position);
     }
