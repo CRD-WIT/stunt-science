b2c3565bac2c51bf84ac822bedfdb921ef1afa63
Mike <mhermogila@gmail.com>
2021-04-07 16:15:03 +0800

    updated distance annotation

--- Assets/Scripts/Mike/DimensionManager.cs
+++ Assets/Scripts/Mike/DimensionManager.cs
@@ -7,6 +7,7 @@ public class DimensionManager : MonoBehaviour
 {
     public TMP_Text dimensionText;
     public LineRenderer dimensionLine;
+    public GameObject endAnnotationArrow;
     public static float dimensionLength;
     Vector3[] position;
     // Start is called before the first frame update
@@ -18,22 +19,27 @@ public class DimensionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // float YConstant = 0.75f;
         float XDistanceFromGround = 0.75f;
-        dimensionText.text = dimensionLength.ToString();
-        dimensionText.transform.position = new Vector3(dimensionLength / 2, XDistanceFromGround, 0);
-        // position = new Vector3[6];
-        // position[0] = new Vector3(0, YConstant, 0);
-        // position[1] = new Vector3(0, -YConstant, 0);
-        // position[2] = new Vector3(0, XDistanceFromGround, 0);
-        // position[3] = new Vector3(dimensionLength, XDistanceFromGround, 0);
-        // position[4] = new Vector3(dimensionLength, YConstant, 0);
-        // position[5] = new Vector3(dimensionLength, -YConstant, 0);
-        position = new Vector3[2];
-        position[0] = new Vector3(0, XDistanceFromGround, 0);
-        position[1] = new Vector3(dimensionLength, XDistanceFromGround, 0);
+        switch(this.gameObject.name){
+            case "AnnotationLength":
+                position = new Vector3[2];
+                position[0] = new Vector3(0, XDistanceFromGround, 0);
+                position[1] = new Vector3(dimensionLength, XDistanceFromGround, 0);
 
-        dimensionLine.positionCount = position.Length;
-        dimensionLine.SetPositions(position);
+                dimensionLine.positionCount = position.Length;
+                dimensionLine.SetPositions(position);
+            break;
+            case "EndAnnotationLine":
+                position = new Vector3[2];
+                position[0] = new Vector3(dimensionLength, -XDistanceFromGround, 0);
+                position[1] = new Vector3(dimensionLength, 1.5f, 0);
+
+                dimensionLine.positionCount = position.Length;
+                dimensionLine.SetPositions(position);
+            break;
+        }        
+        dimensionText.text = dimensionLength.ToString()+"m";
+        dimensionText.transform.position = new Vector3(dimensionLength / 2, XDistanceFromGround, 0);
+        endAnnotationArrow.transform.position = new Vector3(dimensionLength,0.75f,0);        
     }
 }
