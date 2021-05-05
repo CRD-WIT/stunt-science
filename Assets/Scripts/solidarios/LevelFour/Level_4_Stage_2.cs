c2ef1e8ffcd6e34e981e09df6b93fbbf9339ffd3
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-05-05 13:52:55 +0800

    Added new prefab for rectangle annotations.

--- Assets/Scripts/solidarios/LevelFour/Level_4_Stage_2.cs
+++ Assets/Scripts/solidarios/LevelFour/Level_4_Stage_2.cs
@@ -7,43 +7,20 @@ public class Level_4_Stage_2 : MonoBehaviour
 {
     // Start is called before the first frame update
     string question;
-    public float elapsed;
-
-    public TMP_Text initalVelociyText, playerNameText, stuntMessageText, timerText, questionText, levelName;
+    public TMP_Text questionText, levelName;
     public GameObject AfterStuntMessage;
     public GameObject movingToHookHand;
     Animator thePlayerAnimation;
-    bool isMovingToHook;
-    public Animator levelAnimations;
-    public TMP_InputField playerAnswer;
     public GameObject hook;
     bool isSimulating = false;
-    public float speed = 1f;
     public GameObject thePlayer;
-    public GameObject FirstCamera;
-    public GameObject thinRope;
-    public GameObject SecondCamera;
     Vector3 thePlayer_position;
-    public GameObject Rope2HookEnd;
-    public GameObject HookAttachmentCollider;
     float timeGiven;
     Vector2 gravityGiven;
-    float correctAnswer;
     Vector2 spawnPointValue;
     public GameObject hookLauncher;
-    public GameObject hookLine;
-    public GameObject hookIndicator;
-    public TextMeshPro timeIndicator;
-    float distance;
     float distanceGiven;
-    bool doneFiring = false;
     float angleGiven;
-    float velocityX = 0;
-    float velocityY = 0;
-    float velocityInitial = 0;
-    float totalRopeMass = 0f;
-    float timeOfFlight = 0f;
-    public GameObject angularAnnotation;
 
     void Start()
     {
@@ -91,43 +68,17 @@ public class Level_4_Stage_2 : MonoBehaviour
 
     void GenerateVelocities()
     {
-        velocityX = Mathf.Sqrt(Mathf.Abs((distanceGiven * gravityGiven.y) / (2 * Mathf.Tan(angleGiven * Mathf.Deg2Rad))));
-        velocityInitial = Mathf.Abs((velocityX / Mathf.Cos(angleGiven * Mathf.Deg2Rad)));
-        velocityY = Mathf.Abs(velocityInitial * Mathf.Sin(angleGiven * Mathf.Deg2Rad));
 
-        initalVelociyText.SetText($"{System.Math.Round(velocityInitial, 2)} m/s");
-
-        // Formula
-        timeOfFlight = Mathf.Abs((2 * velocityInitial * Mathf.Sin(angleGiven * Mathf.Deg2Rad)) / gravityGiven.y);
-
-        correctAnswer = timeOfFlight;
-
-        Debug.Log($"Angle: {gravityGiven.y}");
-        Debug.Log($"Angle: {angleGiven}");
-        Debug.Log($"VelocityX: {velocityX}");
-        Debug.Log($"VelocityY: {velocityY}");
-        Debug.Log($"VelocityInitial: {velocityInitial}");
-        Debug.Log($"TimeofFlight: {timeOfFlight}");
     }
 
     IEnumerator DropRope()
     {
-        yield return new WaitForSeconds(1.5f);
-        hookLauncher.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-
+        yield return new WaitForSeconds(1.5f);      
     }
 
     IEnumerator PullRope()
     {
-        yield return new WaitForSeconds(0.3f);
-        isMovingToHook = true;
-        thinRope.gameObject.SetActive(false);
-        hookLine.SetActive(true);
-        levelAnimations.SetBool("isMoving", true);
-        thePlayerAnimation.SetBool("isPullingRope", true);
-        yield return new WaitForSeconds(1.59f);
-        thePlayerAnimation.SetBool("happy", true);
-        hookLine.SetActive(false);
+        yield return new WaitForSeconds(1.3f);
     }
 
     public void StartSimulation()
@@ -136,129 +87,6 @@ public class Level_4_Stage_2 : MonoBehaviour
     }
     void FixedUpdate()
     {
-        float thePlayer_x = thePlayer_position.x;
-        float thePlayer_y = thePlayer_position.y;
 
-        if (velocityX == 0 || velocityY == 0 || velocityInitial == 0)
-        {
-            GenerateVelocities();
-        }
-
-        if (isMovingToHook && !isSimulating)
-        {
-            hookLine.GetComponent<LineRenderer>().SetPosition(0, movingToHookHand.transform.position);
-            hookLine.GetComponent<LineRenderer>().SetPosition(1, hook.transform.position);
-        }
-        else
-        {
-            hookLine.GetComponent<LineRenderer>().SetPosition(0, hookLauncher.transform.position);
-            hookLine.GetComponent<LineRenderer>().SetPosition(1, hook.transform.position);
-        }
-
-        if (isSimulating)
-        {
-
-            if (playerAnswer.text.Length > 0)
-            {
-                timeIndicator.transform.position = new Vector3(hook.transform.position.x, hook.transform.position.y + 1, 1);
-                transform.Find("Annotation1").GetComponent<Annotation>().Hide();
-                transform.Find("CircularAnnotation").GetComponent<CircularAnnotation>().Hide();
-                transform.Find("AngularAnnotation").GetComponent<AngularAnnotation>().Hide();
-
-                elapsed += Time.fixedDeltaTime;
-                timerText.text = elapsed.ToString("f2") + "s";
-                timeIndicator.text = elapsed.ToString("f2") + "s";
-
-                // Correct Answer
-                if (System.Math.Round(float.Parse(playerAnswer.text), 2) == System.Math.Round(correctAnswer, 2))
-                {
-                    if (!doneFiring)
-                    {
-                        hook.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                        hook.GetComponent<Rigidbody2D>().WakeUp();
-                        hook.GetComponent<Rigidbody2D>().velocity = new Vector3(velocityX, velocityY, 0) / (hook.GetComponent<Rigidbody2D>().mass);
-                        doneFiring = true;
-                    }
-
-                    if (hook.GetComponent<Hook>().isCollided)
-                    {
-                        thinRope.gameObject.SetActive(true);
-                        hookLine.SetActive(false);
-                        elapsed -= 0.01f;
-                        isSimulating = false;
-
-                        Rope2HookEnd.GetComponent<Rigidbody2D>().velocity = new Vector3(2, 0, 0);
-                        StartCoroutine(PullRope());
-                    }
-                }
-                else
-                {
-                    if (System.Math.Round(correctAnswer, 2) > float.Parse(playerAnswer.text))
-                    {
-                        // Too short
-                        Debug.Log("Too short");
-                        HookAttachmentCollider.SetActive(false);
-
-                        if (!doneFiring)
-                        {
-                            hook.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                            hook.GetComponent<Rigidbody2D>().WakeUp();
-                            hook.GetComponent<Rigidbody2D>().velocity = new Vector3(velocityX, velocityY, 0) / (hook.GetComponent<Rigidbody2D>().mass);
-                            doneFiring = true;
-                        }
-
-                        if (hook.GetComponent<Hook>().isCollidedToFailCollider)
-                        {
-                            isSimulating = false;
-
-                        }
-
-                    }
-                    else
-                    {
-                        // Too long
-                        Debug.Log("Too long");
-                        HookAttachmentCollider.SetActive(false);
-
-                        if (!doneFiring)
-                        {
-                            hook.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                            hook.GetComponent<Rigidbody2D>().WakeUp();
-                            hook.GetComponent<Rigidbody2D>().velocity = new Vector3(velocityX, velocityY, 0) / (hook.GetComponent<Rigidbody2D>().mass);
-                            doneFiring = true;
-                        }
-
-                        if (hook.GetComponent<Hook>().isCollidedToFailCollider)
-                        {
-                            isSimulating = false;
-
-                        }
-
-                    }
-                }
-            }
-            else
-            {
-                Debug.Log("No value was added");
-            }
-        }
-        else
-        {
-            //platformBarBottom.transform.position = new Vector3(spawnPointValue.x - 9, playerOnRope.transform.Find("PlayerHingeJoint").transform.position.y - distance, 1);            
-
-
-            if (hook.GetComponent<Hook>().isCollidedToFailCollider)
-            {
-                hook.transform.position -= new Vector3(0.1f, -0.1f, 0);
-                hookIndicator.SetActive(false);
-                StartCoroutine(DropRope());
-                thePlayerAnimation.SetBool("lost", true);
-            }
-
-
-            timerText.text = $"{(elapsed).ToString("f2")}s";
-            timeIndicator.text = $"{(elapsed).ToString("f2")}s";
-
-        }
     }
 }
