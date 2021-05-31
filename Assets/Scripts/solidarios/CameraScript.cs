fb9df7b157b7ea828f293f8496cda3c1853ae564
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-05-31 11:57:20 +0800

    Added new callout

--- Assets/Scripts/solidarios/CameraScript.cs
+++ Assets/Scripts/solidarios/CameraScript.cs
@@ -1,12 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CameraScript : MonoBehaviour
 {
     GameObject cameraImage;
     GameObject spotLight;
     public GameObject target;
+    [SerializeField] bool isCalloutOpen;
+    [SerializeField] GameObject callout;
+    [SerializeField] TMP_Text calloutText;
+    [SerializeField] QuestionController questionController;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +20,38 @@ public class CameraScript : MonoBehaviour
 
         cameraImage.GetComponent<LookAtConstraint2D>().SetTarget(target);
         spotLight.GetComponent<LookAtConstraint2D>().SetTarget(target);
+    }
+
+    public IEnumerator DirectorsCall()
+    {
+        if (questionController.isSimulating)
+        {
+            calloutText.SetText("Lights!");
+            yield return new WaitForSeconds(0.75f);
+            calloutText.SetText("Camera!");
+            yield return new WaitForSeconds(0.75f);
+            calloutText.SetText("Action!");
+            yield return new WaitForSeconds(0.75f);
+            calloutText.SetText("");
+        }
+        else
+        {
+            calloutText.SetText("Cut!");
+            yield return new WaitForSeconds(0.75f);
+            isCalloutOpen = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isCalloutOpen)
+        {
+            callout.SetActive(true);
+            StartCoroutine(DirectorsCall());
+        }
+        else
+        {
+            callout.SetActive(false);
+        }
     }
 }
