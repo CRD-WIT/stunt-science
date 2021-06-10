cf708bbe12eee3c37cd692efe2556368ff4c1cb7
biboy <junejaspe@gmail.com>
2021-06-10 17:01:26 +0800

    10-6-21

--- Assets/Scripts/bibpyScript/AccHard/AccHardOne.cs
+++ Assets/Scripts/bibpyScript/AccHard/AccHardOne.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class AccHardOne : MonoBehaviour
 {
-    public float xg, xy, viT, aT, time, vB, angleB;
+    public float dX, dY, viT, aT, timer, vB, angleB, angleA, sideB, sideC, totalDistance, correctAnswer;
     //public Quaternion angleB;
-    public GameObject gunBarrel,gun;
+    public GameObject gunBarrel,gun, target, targetWheel;
+    public GameObject verticalOne, horizontal;
     public TruckManager theTruck;
     public Hellicopter theChopper;
     public ShootManager theShoot;
-    float generateAngleB, generateViT, generateAT, generateVB;
+    public DistanceMeter[] theMeter;
+    public CircularAnnotation theCurve;
+    float generateAngleB, generateViT, generateAT, generateVB, generateDX, generateDY;
+    float ChopperY, chopperX, truckTime,bulletTime;
+    bool shoot;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,16 +24,37 @@ public class AccHardOne : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        gun.transform.rotation = Quaternion.Euler(gun.transform.rotation.x,gun.transform.rotation.y,angleB);
+    { 
         theShoot.speed = vB;
-        theTruck.accelerating = true;
-        theTruck.moveSpeed = viT;
-        theTruck.accelaration = aT;
+        target.transform.position = targetWheel.transform.position;
+        sideB = (Mathf.Tan(angleA * Mathf.Deg2Rad)) *dY;
+        sideC = Mathf.Sqrt((dY*dY)+(sideB * sideB));
+        totalDistance = sideB + dX;
+        truckTime = (-viT + Mathf.Sqrt((viT*viT) + (4*((aT/2)* totalDistance)))) / aT;
+        bulletTime = sideC/ vB;
+        correctAnswer = truckTime-bulletTime;
+        if(AccHardSimulation.simulate == true)
+        {
+            timer += Time.fixedDeltaTime;
+            theTruck.accelerating = true;
+            theTruck.moveSpeed = viT;
+            theTruck.accelaration = aT;
+            if(timer >= correctAnswer)
+            {
+                shoot = true;
+            }
+        }
+        if(shoot)
+        {
+            theShoot.Shoot();
+        }
     }
     public void generateProblem()
     {
-        generateAngleB = Random.Range(-20f, -60f);
+        shoot = false;
+        dX = Random.Range(9,12);
+        dY = Random.Range(10,12);
+        generateAngleB = Random.Range(-20f, -30f);
         angleB =(float)System.Math.Round(generateAngleB, 2);
         generateViT = Random.Range(3f,5f);
         viT = (float)System.Math.Round(generateViT, 2);
@@ -36,5 +62,22 @@ public class AccHardOne : MonoBehaviour
         aT = (float)System.Math.Round(generateAT, 2);
         generateVB = Random.Range(20f, 30f);
         vB = (float)System.Math.Round(generateVB, 2);
+        gun.transform.rotation = Quaternion.Euler(gun.transform.rotation.x,gun.transform.rotation.y,angleB);
+        ChopperY = theChopper.transform.position.y - gunBarrel.transform.position.y;
+        chopperX = gunBarrel.transform.position.x - theChopper.transform.position.x;
+        theChopper.transform.position = new Vector2(targetWheel.transform.position.x + dX - chopperX, targetWheel.transform.position.y + dY + ChopperY);
+        horizontal.transform.position = new Vector2(horizontal.transform.position.x, gunBarrel.transform.position.y);
+        verticalOne.transform.position = new Vector2(gunBarrel.transform.position.x, verticalOne.transform.position.y);
+        theMeter[1].distance = dY;
+        theMeter[1].positionX = targetWheel.transform.position.x;
+        theMeter[1].positionY = targetWheel.transform.position.y;
+        theMeter[0].distance = dX;
+        theMeter[0].positionX = targetWheel.transform.position.x;
+        theMeter[0].positionY =dY - 5;
+        theCurve._origin = new Vector2 (gunBarrel.transform.position.x +.3f, targetWheel.transform.position.y + dY -.2f);
+        theCurve._degrees = -angleB;
+        angleA = 90+angleB;
+        
+
     }
 }
--- Assets/Scripts/bibpyScript/DistanceMeter.cs
+++ Assets/Scripts/bibpyScript/DistanceMeter.cs
@@ -4,40 +4,68 @@ using UnityEngine;
 using TMPro;
 public class DistanceMeter : MonoBehaviour
 {
-    public GameObject  d1, d2, edge,mother;
+    public GameObject d1, d2, edge, mother;
     public TMP_Text distancetxt;
     public float positionX, positionY, distance;
     float d1Scale, d2Pos, d2Scale, edgePos;
+    public bool horizontal, vertical;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        edgePos = positionX + distance;
-        d1Scale = (distance/2) - 2.5f;
-        d2Pos = positionX + ((distance/2) + 2);
-        d2Scale = (distance/2) - 2.5f;
-        if (d1Scale < 0)
+        if (horizontal)
         {
-            d1Scale = 0;
+            edgePos = positionX + distance;
+            d1Scale = (distance / 2) - 2.5f;
+            d2Pos = positionX + ((distance / 2) + 2);
+            d2Scale = (distance / 2) - 2.5f;
+            if (d1Scale < 0)
+            {
+                d1Scale = 0;
+            }
+            if (d2Scale < 0)
+            {
+                d2Scale = 0;
+            }
+            mother.transform.position = new Vector2(positionX, positionY);
+            edge.transform.position = new Vector2(edgePos, edge.transform.position.y);
+            d1.transform.localScale = new Vector2(d1Scale, d1.transform.localScale.y);
+            d1.transform.position = new Vector2(positionX + .5f, d1.transform.position.y);
+            d2.transform.position = new Vector2(d2Pos, d2.transform.position.y);
+            d2.transform.localScale = new Vector2(d2Scale, d2.transform.localScale.y);
+            distancetxt.text = distance.ToString("F2") + ("m");
+            distancetxt.gameObject.transform.position = new Vector2(positionX + (distance / 2), distancetxt.gameObject.transform.position.y);
         }
-        if (d2Scale < 0)
-        {
-            d2Scale = 0;
+        if(vertical)
+        {   mother.transform.rotation = Quaternion.Euler(mother.transform.rotation.x,mother.transform.rotation.y,90);
+            edgePos = positionY+ distance;
+            d1Scale = (distance / 2) - 1f;
+            d2Pos = positionY + ((distance / 2) + 1);
+            d2Scale = (distance / 2) - 1.5f;
+            if (d1Scale < 0)
+            {
+                d1Scale = 0;
+            }
+            if (d2Scale < 0)
+            {
+                d2Scale = 0;
+            }
+            mother.transform.position = new Vector2(positionX, positionY);
+            edge.transform.position = new Vector2(edge.transform.position.x, edgePos);
+            d1.transform.localScale = new Vector2(d1Scale, d1.transform.localScale.y);
+            d1.transform.position = new Vector2( d1.transform.position.x, positionY + .5f);
+            d2.transform.position = new Vector2(d2.transform.position.x, d2Pos);
+            d2.transform.localScale = new Vector2(d2Scale, d2.transform.localScale.y);
+            distancetxt.text = distance.ToString("F2") + ("m");
+            distancetxt.gameObject.transform.rotation =  Quaternion.Euler(distancetxt.gameObject.transform.rotation.x,distancetxt.gameObject.transform.rotation.y,0);
+            distancetxt.gameObject.transform.position = new Vector2(distancetxt.gameObject.transform.position.x, positionY + (distance / 2));
         }
-        mother.transform.position = new Vector2(positionX, positionY);
-        edge.transform.position = new Vector2(edgePos, edge.transform.position.y);
-        d1.transform.localScale = new Vector2(d1Scale, d1.transform.localScale.y);
-        d1.transform.position = new Vector2(positionX, d1.transform.position.y);
-        d2.transform.position = new Vector2(d2Pos, d2.transform.position.y);
-        d2.transform.localScale = new Vector2(d2Scale, d2.transform.localScale.y);
-        distancetxt.text = distance.ToString("F2") + ("m");
-        distancetxt.gameObject.transform.position = new Vector2(positionX + (distance/2), distancetxt.gameObject.transform.position.y);
-        
+
     }
 }
