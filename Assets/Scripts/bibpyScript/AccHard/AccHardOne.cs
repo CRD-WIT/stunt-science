02598ab71ca122abc8bb02384e8049d37c18460c
biboy <junejaspe@gmail.com>
2021-06-14 17:00:47 +0800

    14-6-21accHard

--- Assets/Scripts/bibpyScript/AccHard/AccHardOne.cs
+++ Assets/Scripts/bibpyScript/AccHard/AccHardOne.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AccHardOne : MonoBehaviour
 {
@@ -8,26 +9,33 @@ public class AccHardOne : MonoBehaviour
     //public Quaternion angleB;
     public GameObject gunBarrel, gun, target, targetWheel, projectileLine, dimensions;
     public GameObject verticalOne, horizontal;
+    public GameObject bulletPos, wheelPos, bulletHere, targetHere;
     public TruckManager theTruck;
     public Hellicopter theChopper;
     public ShootManager theShoot;
     public DistanceMeter[] theMeter;
     public CircularAnnotation theCurve;
     public QuestionController theQuestion;
+    private BulletManager theBullet;
+    public AccHardSimulation theSimulate;
     float generateAngleB, generateViT, generateAT, generateVB, generateDX, generateDY;
     float ChopperY, chopperX, truckTime, bulletTime;
     bool shoot, shootReady, gas;
+    public bool posCheck;
+     public TMP_Text timertxt, timertxtTruck;
     // Start is called before the first frame update
     void Start()
     {
+
         generateProblem();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        theBullet= FindObjectOfType<BulletManager>();
         theShoot.speed = vB;
-        target.transform.position = targetWheel.transform.position;
+        
         sideB = (Mathf.Tan(angleA * Mathf.Deg2Rad)) * dY;
         sideC = Mathf.Sqrt((dY * dY) + (sideB * sideB));
         totalDistance = sideB + dX;
@@ -37,9 +45,10 @@ public class AccHardOne : MonoBehaviour
         answer = (float)System.Math.Round(correctAnswer, 2);
         playerAnswer = AccHardSimulation.playerAnswer;
 
+
         if (AccHardSimulation.simulate == true)
         {
-            projectileLine.SetActive(false);
+            target.transform.position = targetWheel.transform.position;
             dimensions.SetActive(false);
             if (timer == 0)
             {
@@ -48,10 +57,12 @@ public class AccHardOne : MonoBehaviour
                 theTruck.accelerating = true;
             }
             timer += Time.fixedDeltaTime;
+            timertxt.text = timer.ToString("F2") + ("s");
             if (playerAnswer == answer)
             {
                 if (timer >= correctAnswer + .05f)
                 {
+
                     shoot = true;
 
                 }
@@ -61,7 +72,6 @@ public class AccHardOne : MonoBehaviour
                 if (timer >= playerAnswer + .1f)
                 {
                     shoot = true;
-
                 }
             }
             if (playerAnswer < answer)
@@ -69,18 +79,39 @@ public class AccHardOne : MonoBehaviour
                 if (timer >= playerAnswer - .1f)
                 {
                     shoot = true;
-
                 }
             }
         }
 
         if (shoot)
         {
+            AccHardSimulation.simulate = false;
+            timertxt.text = playerAnswer.ToString("F2");
             if (shootReady)
             {
                 theShoot.Shoot();
                 shootReady = false;
-                target.SetActive(false);
+               
+            }
+            if (posCheck)
+            {
+                if (playerAnswer != answer)
+                {
+                    bulletPos.SetActive(true);
+                    bulletPos.transform.position = theBullet.gameObject.transform.position;
+                    bulletPos.transform.rotation = theBullet.gameObject.transform.rotation;
+                    bulletHere.SetActive(true);
+                    bulletHere.transform.position = bulletPos.transform.position;
+                    wheelPos.SetActive(true);
+                    wheelPos.transform.position = targetWheel.transform.position;
+                    target.transform.position = wheelPos.transform.position;
+                    targetHere.SetActive(true);
+                    targetHere.transform.position = wheelPos.transform.position;
+                }
+                theTruck.accelerating = false;
+                theTruck.deaccelerating = true;
+                StartCoroutine(StuntResult());
+                shoot = false;
             }
 
         }
@@ -117,7 +148,19 @@ public class AccHardOne : MonoBehaviour
         theCurve._degrees = angleB;
         angleA = 90 - angleB;
         theQuestion.SetQuestion((("<b>") + PlayerPrefs.GetString("Name") + ("</b> is now instructed to shoot the hub or the center of the moving truck's wheel from a non moving helicopter. If at time = Φ, the hub is <b>") + dX.ToString("F2") + ("</b> meters horizontally behind and <b>") + dY.ToString("F2") + ("</b> meters vertically above the tip of the gun barrel that <b>") + PlayerPrefs.GetString("Name") + ("</b> holding, at how many seconds after should  <b>") + PlayerPrefs.GetString("Name") + ("</b> shoots if the truck has an initial velocity of <b>") + viT.ToString("F2") + ("</b> m/s and accelerating at <b>") + aT.ToString("F2") + ("</b> m/s² while the gun is aimed <b>") + angleB.ToString("F2") + ("</b> degrees below the horizon and its bullet travels at a constant velocity of <b>") + vB.ToString("F2") + ("</b> m/s?")));
+        target.transform.position = targetWheel.transform.position;
 
-
+    }
+    IEnumerator StuntResult()
+    {
+        yield return new WaitForSeconds(.5f);
+        StartCoroutine(theSimulate.DirectorsCall());
+        yield return new WaitForSeconds(.5f);
+        theQuestion.ToggleModal();
+        theTruck.deaccelerating = false;
+        if (playerAnswer == answer)
+        {
+            Time.timeScale = 0;
+        }
     }
 }
