30ee568db01957840ffa97dfb481f0055aad966e
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2021-07-15 15:44:03 +0800

    Revised level selector

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    [Range(0, 100)]
+
+    [Range(0, 16)]
     public int unlockedCount = 0;
+    [Header("Velocity")]
+
+    [Range(0, 3)] public int levelVelocityEasy = 0;
+    [Range(0, 3)] public int levelVelocityMedium = 0;
+    [Range(0, 3)] public int levelVelocityHard = 0;
+
+    [Header("Acceleration")]
+    [Range(0, 3)] public int levelAccelerationEasy = 0;
+    [Range(0, 3)] public int levelAccelerationMedium = 0;
+    [Range(0, 3)] public int levelAccelerationHard = 0;
+    [Header("Free Fall and Projectile")]
+    [Range(0, 3)] public int levelFreeFallProjectileEasy = 0;
+    [Range(0, 3)] public int levelFreeFallProjectileMedium = 0;
+    [Range(0, 3)] public int levelFreeFallProjectileHard = 0;
+    [Header("Circular Motion")]
+    [Range(0, 3)] public int levelCircularMotionEasy = 0;
+    [Range(0, 3)] public int levelCircularMotionMedium = 0;
+    [Range(0, 3)] public int levelCircularMotionHard = 0;
+    [Header("Forces")]
+    [Range(0, 3)] public int levelForcesEasy = 0;
+    [Range(0, 3)] public int levelForcesMedium = 0;
+    [Range(0, 3)] public int levelForcesHard = 0;
     public LevelCard[] levelCards;
     void Start()
     {
@@ -17,8 +40,23 @@ public class LevelManager : MonoBehaviour
     {
         for (int i = 0; i < levelCards.Length; i++)
         {
-            levelCards[i].locked = unlockedCount <=i+1;
+            levelCards[i].locked = unlockedCount <= i + 1;
         }
-        
+        levelCards[0].starActiveCount = levelVelocityEasy;
+        levelCards[1].starActiveCount = levelVelocityMedium;
+        levelCards[2].starActiveCount = levelVelocityHard;
+        levelCards[3].starActiveCount = levelAccelerationEasy;
+        levelCards[4].starActiveCount = levelAccelerationMedium;
+        levelCards[5].starActiveCount = levelAccelerationHard;
+        levelCards[6].starActiveCount = levelFreeFallProjectileEasy;
+        levelCards[7].starActiveCount = levelFreeFallProjectileMedium;
+        levelCards[8].starActiveCount = levelFreeFallProjectileHard;
+        levelCards[9].starActiveCount = levelCircularMotionEasy;
+        levelCards[10].starActiveCount = levelCircularMotionMedium;
+        levelCards[11].starActiveCount = levelCircularMotionHard;
+        levelCards[12].starActiveCount = levelForcesEasy;
+        levelCards[13].starActiveCount = levelForcesMedium;
+        levelCards[14].starActiveCount = levelForcesHard;
+
     }
 }
