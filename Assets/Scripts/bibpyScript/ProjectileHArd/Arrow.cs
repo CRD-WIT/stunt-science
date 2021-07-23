606cce18d8143a717af5a2eb8cb9540dc8260e21
biboy <junejaspe@gmail.com>
2021-07-23 16:49:21 +0800

    23-7-21

--- Assets/Scripts/bibpyScript/ProjectileHArd/Arrow.cs
+++ Assets/Scripts/bibpyScript/ProjectileHArd/Arrow.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    private ProjectileHardOne theManagerOne;
     Rigidbody2D rb;
+    float mylastrotX, mylastrotY, angle, newAngle;
+    bool getAngle;
+    Quaternion lastRot;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        theManagerOne = FindObjectOfType<ProjectileHardOne>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if(!getAngle)
+        {
+            angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            lastRot = transform.rotation;
+        }
+        
+        
+    }
+     void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+        if (other.gameObject.tag == ("wall"))
+        {  
+            theManagerOne.hit = true;
+            getAngle = true;        
+            rb.bodyType = RigidbodyType2D.Static;
+            transform.rotation = lastRot;
+        }
     }
 }
--- Assets/Scripts/bibpyScript/ProjectileHArd/ProjectileHardOne.cs
+++ Assets/Scripts/bibpyScript/ProjectileHArd/ProjectileHardOne.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class ProjectileHardOne : MonoBehaviour
 {
+    public PlayerB thePlayer;
     public ShootManager theShoot;
     public GameObject Mgear, arrowPoint,rangePoint,stone;
     public float arrowVi;
     public float generateAngle, HRange;
     public float stoneDY,stoneDX,stoneAngle, stoneDH;
+    public bool hit;
 
     // Start is called before the first frame update
     void Start()
@@ -25,15 +27,26 @@ public class ProjectileHardOne : MonoBehaviour
         stoneAngle = Mathf.Asin(stoneDY/stoneDH)* Mathf.Rad2Deg;
        
         Mgear.transform.rotation = Quaternion.Euler(Mgear.transform.rotation.x, Mgear.transform.rotation.y, generateAngle);
+        transform.rotation =  Quaternion.Euler(transform.rotation.x, transform.rotation.y, generateAngle);
         //HRange = ((arrowVi*arrowVi) * (Mathf.Sin((generateAngle*2) * Mathf.Deg2Rad)))/ Mathf.Abs(Physics2D.gravity.y);
         rangePoint.transform.position = new Vector2(theShoot.transform.position.x +stoneDH, theShoot.transform.position.y);
         //theShoot.speed = Mathf.Sqrt((stoneDH*Mathf.Abs(Physics2D.gravity.y)) / (Mathf.Sin((generateAngle*2) * Mathf.Deg2Rad)));
-        arrowVi = Mathf.Sqrt((stoneDY* 2 * Mathf.Cos((generateAngle*generateAngle)* Mathf.Deg2Rad))/ Mathf.Tan(generateAngle * Mathf.Deg2Rad)* stoneDX*stoneDX*stoneDX*Mathf.Abs(Physics2D.gravity.y));
+        arrowVi = ((Mathf.Sqrt((Mathf.Abs(Physics2D.gravity.y)/(2 * ((stoneDX * (Mathf.Tan((generateAngle) * Mathf.Deg2Rad))) - stoneDY))))) * stoneDX) / (Mathf.Cos((generateAngle)* Mathf.Deg2Rad)) ;
         theShoot.speed = arrowVi;
+        if(hit)
+        {
+            StartCoroutine(ropePull());
+        }
     }
     public void generateProblem()
     {
         //arrowVi = Random.Range(10,15);
-        generateAngle = Random.Range(30,45);
+        generateAngle = Random.Range(30,25);
+    }
+    IEnumerator ropePull()
+    {
+        yield return new WaitForSeconds(3);
+        thePlayer.moveSpeed = arrowVi;
+       
     }
 }
