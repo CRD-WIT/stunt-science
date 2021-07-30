50d56260e603f82d604e60969900d848ef827c4c
maicpol <mhermogila@gmail.com>
2021-07-30 15:22:08 +0800

    Implementation of indicators done

--- Assets/Prefabs/Mike/TestingScript.cs
+++ Assets/Prefabs/Mike/TestingScript.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 using GameConfig;
 using TMPro;
 
-public class SlantingIndicator : MonoBehaviour
+public class TestingScript : MonoBehaviour
 {
     TextColorMode distanceColor, velocityColor;
     [SerializeField] float fontSize;
     [Header("Line Position")]
     [Space(2)]
-    [SerializeField] public Vector2 distanceSpawnPnt;
+    [SerializeField] public Vector2 distanceSpawnPnt = new Vector2(0, 0);
     [Header("Line Legnth")]
     [Space(2)]
-    public float distance, angle, x, y;
+    public float distance = 20, angle, x, y;
     float dimensionTxtLength, velocityTxtLength;
     Vector2 velocitySpawnPnt;
     [SerializeField] LineRenderer[] distanceLines = new LineRenderer[4];
@@ -26,46 +26,64 @@ public class SlantingIndicator : MonoBehaviour
     {
         qc = FindObjectOfType<QuestionControllerVThree>();
     }
-    public void SetDistance(float distance, float angle, float x, float y)
-    {
-        if (arrowPresent)
-        {
-            foreach (var item in arrows)
-                Destroy(item);
-            arrowPresent = false;
-            if (!arrowPresent)
-            {
-                for (int i = 0; i <= 3; i++)
-                {
-                    arrows[i] = Instantiate(arrow);
-                    arrows[i].transform.SetParent(transform.Find("Arrows"));
-                }
-                arrows[0].transform.Rotate(0, 0, angle);
-            }
-            distanceColor = TextColorMode.Given;
-        }
-    }
-    public void SetSpawnPnt(Vector2 pos)
-    {
-        this.distanceSpawnPnt = pos;
-    }
+    // public void SetDistance(float distance, float angle, float x, float y)
+    // {
+    //     if (arrowPresent)
+    //     {
+    //         foreach (var item in arrows)
+    //             Destroy(item);
+    //         arrowPresent = false;
+    //         if (!arrowPresent)
+    //         {
+    //             for (int i = 0; i <= 3; i++)
+    //             {
+    //                 arrows[i] = Instantiate(arrow);
+    //                 arrows[i].transform.SetParent(transform.Find("Arrows"));
+    //             }
+    //             arrows[0].transform.Rotate(0, 0, angle);
+    //         }
+    //         distanceColor = TextColorMode.Given;
+    //     }
+    // }
+    // public void SetSpawnPnt(Vector2 pos)
+    // {
+    //     this.distanceSpawnPnt = pos;
+    // }
     private void Update()
     {
         x = Mathf.Sin(Mathf.Deg2Rad * angle) * distance;
         y = Mathf.Cos(Mathf.Deg2Rad * angle) * distance;
 
+        // if (arrowPresent)
+        // {
+        //     foreach (var item in arrows)
+        //         Destroy(item);
+        //     arrowPresent = false;
+        // }
+        if (!arrowPresent)
+        {
+            arrowPresent =true;
+            for (int i = 0; i <= 3; i++)
+            {
+                arrows[i] = Instantiate(arrow);
+                arrows[i].transform.SetParent(transform.Find("Arrows"));
+            }
+            arrows[0].transform.Rotate(0, 0, angle);
+        }
+        distanceColor = TextColorMode.Given;
+
         dimensionTxtLength = labelTxt[0].GetComponent<TextMeshPro>().text.Length;
 
         arrows[0].transform.position = new Vector2(x, distanceSpawnPnt.y);
-        arrows[1].transform.position = new Vector2(distanceSpawnPnt.x, distanceSpawnPnt.y);
+        // arrows[1].transform.position = new Vector2(distanceSpawnPnt.x, distanceSpawnPnt.y);
         distanceLines[0].SetPosition(0, new Vector2(((x / 2) - (0.18f * dimensionTxtLength)) + distanceSpawnPnt.x, ((y / 2) - (0.18f * dimensionTxtLength)) + distanceSpawnPnt.y));
         distanceLines[0].SetPosition(1, new Vector2(distanceSpawnPnt.x, distanceSpawnPnt.y));
-        distanceLines[1].SetPosition(0, new Vector2(((x / 2) + (0.18f * dimensionTxtLength)) + distanceSpawnPnt.x, ((y / 2) + (0.18f * dimensionTxtLength)) + distanceSpawnPnt.y));
-        distanceLines[1].SetPosition(1, new Vector2((x + distanceSpawnPnt.x), distanceSpawnPnt.y));
-        distanceLines[2].SetPosition(0, new Vector2(distanceSpawnPnt.x, distanceSpawnPnt.y - 1f));
-        distanceLines[2].SetPosition(1, new Vector2(distanceSpawnPnt.x, distanceSpawnPnt.y + 0.5f));
-        distanceLines[3].SetPosition(0, new Vector2(distanceSpawnPnt.x + x, distanceSpawnPnt.y - 1f));
-        distanceLines[3].SetPosition(1, new Vector2(distanceSpawnPnt.x + x, distanceSpawnPnt.y + 0.5f));
+        // distanceLines[1].SetPosition(0, new Vector2(((x / 2) + (0.18f * dimensionTxtLength)) + distanceSpawnPnt.x, ((y / 2) + (0.18f * dimensionTxtLength)) + distanceSpawnPnt.y));
+        // distanceLines[1].SetPosition(1, new Vector2((x + distanceSpawnPnt.x), distanceSpawnPnt.y));
+        // distanceLines[2].SetPosition(0, new Vector2(distanceSpawnPnt.x, distanceSpawnPnt.y - 1f));
+        // distanceLines[2].SetPosition(1, new Vector2(distanceSpawnPnt.x, distanceSpawnPnt.y + 0.5f));
+        // distanceLines[3].SetPosition(0, new Vector2(distanceSpawnPnt.x + x, distanceSpawnPnt.y - 1f));
+        // distanceLines[3].SetPosition(1, new Vector2(distanceSpawnPnt.x + x, distanceSpawnPnt.y + 0.5f));
         labelTxt[0].transform.position = new Vector2((x / 2) + distanceSpawnPnt.x, distanceSpawnPnt.y);
 
         labelTxt[1].transform.position = velocitySpawnPnt;
