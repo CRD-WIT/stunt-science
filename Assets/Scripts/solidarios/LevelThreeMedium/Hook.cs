33e25c72ec0c540560d1800428b92aa0dabdca7a
biboy <junejaspe@gmail.com>
2021-09-21 16:48:55 +0800

    revisons stage1 projectileMedium

--- Assets/Scripts/solidarios/LevelThreeMedium/Hook.cs
+++ Assets/Scripts/solidarios/LevelThreeMedium/Hook.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
-    public bool isCollided;
+    public bool isCollided, isTrailing, getAngle;
     public bool isCollidedToFailCollider;
     public Rigidbody2D rb;
-    public GameObject target, hookLine;
+    Quaternion lastRot;
+     float
+
+            mylastrotX,
+            mylastrotY,
+            angle,
+            newAngle;
+    public GameObject target, hookLine,rope,trail;
     // Start is called before the first frame update
+    void FixedUpdate() 
+    {
+         if (!getAngle)
+        {
+            angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            lastRot = transform.rotation;
+        }
+        if(isTrailing)
+        {
+            trail.transform.position = transform.position;
+        }
+    }
+        
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == ("HookTarget"))
@@ -26,16 +48,21 @@ public class Hook : MonoBehaviour
                 target.GetComponent<Collider2D>().enabled = false;
                 StartCoroutine(ropePull());
                 isCollidedToFailCollider = true;
+                isTrailing = false;
+                getAngle = true;
+               
             }
 
         }
     }
     public IEnumerator ropePull()
     {
-        yield return new WaitForSeconds(2);
-        rb.velocity = new Vector2(-15, rb.velocity.y);
-        yield return new WaitForSeconds(1);
+        //yield return new WaitForSeconds(.7f);
         hookLine.SetActive(false);
+        rope.SetActive(true);
+        yield return new WaitForSeconds(.7f);
+        //rope.SetActive(false);
+       
 
     }
 }
