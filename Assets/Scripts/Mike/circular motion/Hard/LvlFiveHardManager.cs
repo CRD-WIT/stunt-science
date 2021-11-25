35afda5827d81d9f611296c89aec32f5a1db0773
Mike <mhermogila@gmail.com>
2021-11-25 16:57:04 +0800

    Circular Motion: Hard Mecha Updated

--- Assets/Scripts/Mike/circular motion/Hard/LvlFiveHardManager.cs
+++ Assets/Scripts/Mike/circular motion/Hard/LvlFiveHardManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class LvlFiveHardManager : MonoBehaviour
 {
+    [SerializeField]float distance, velocity, aVelocity, radius, time;
+    MechaManager mm;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        mm = FindObjectOfType<MechaManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        mm.SetMechaVelocity(aVelocity, time);
     }
+    //1st gear d =1.18, 2nd gear d= 1.55, 3rd gear d =1.15
 }
--- Assets/Scripts/Mike/circular motion/Hard/MechaManager.cs
+++ Assets/Scripts/Mike/circular motion/Hard/MechaManager.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class MechaManager : MonoBehaviour
 {
-    float velocity;
+    Vector2 velocity;
+    float time;
+    Rigidbody2D[] gearsRB = new Rigidbody2D[3];
+    Rigidbody2D bodyRB;
+    [SerializeField] GameObject[] gears = new GameObject[3];
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i <= 2; i++)
+        {
+            gearsRB[i] = gears[i].GetComponent<Rigidbody2D>();
+        }
+        bodyRB = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.Rigidbody2D.velocity = velocity;
+        bodyRB.velocity = velocity;
+        if(velocity.x != 0) {
+        gearsRB[0].angularVelocity = GearSpeed(-0.575f);
+        gearsRB[1].angularVelocity = GearSpeed(-1.05f);
+        gearsRB[2].angularVelocity = GearSpeed(-0.775f);}
+    }
+    public void SetMechaVelocity(float angularVelocity, float time){
+        float circumferenceOfWheel = 2*(float)(Mathf.PI * (1.05f)),
+        arc = angularVelocity * time,
+        d = circumferenceOfWheel * (arc / 360);
+        velocity = new Vector2 (d/time,0);
+    }
+    float GearSpeed(float radius){
+        return (360*velocity.x/(2*(float)(Mathf.PI*radius)));
     }
 }
