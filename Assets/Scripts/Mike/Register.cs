0e556c6bd3a3444f4ab99541ab0d1a112e232fb5
nicko <wit-crd@WIT-CRDs-iMac.local>
2021-12-21 14:40:07 +0800

    Apple build

--- Assets/Scripts/Mike/Register.cs
+++ Assets/Scripts/Mike/Register.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using Firebase;
-using Firebase.Firestore;
-using Firebase.Extensions;
+// using Firebase;
+// using Firebase.Firestore;
+// using Firebase.Extensions;
 using System.Collections.Generic;
 
 public class Register : MonoBehaviour
@@ -13,8 +13,8 @@ public class Register : MonoBehaviour
     public InputField pCode;
     public Text popUp;
     public WarningErrorUI warningErrorUI;
-    public FirebaseManager firebaseManager;
-    FirebaseApp app;
+    // public FirebaseManager firebaseManager;
+    // FirebaseApp app;
 
     void test()
     {
@@ -27,194 +27,199 @@ public class Register : MonoBehaviour
         warningErrorUI.togglePanel();
     }
 
-    async void RegisterPlayer()
+    public void RegisterPlayer()
     {
-        app = Firebase.FirebaseApp.DefaultInstance;
-        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
-        DocumentReference docRef;
-        docRef = db.Collection("game_keys").Document(pCode.text);
-        Dictionary<string, object> log = new Dictionary<string, object>{
-                            {"binded_device", SystemInfo.deviceUniqueIdentifier},
-                            {"activated", pCode.text=="05ada8"?false:true}
-                        };
-
         Debug.Log("Registering player");
-        PlayerPrefs.SetInt("Life", 3);
-        RegistrationManager.playerName = pName.GetComponent<Text>().text;
-        RegistrationManager.playerCode = pCode.GetComponent<InputField>().text;
-        PlayerPrefs.SetString("Name", RegistrationManager.playerName);
-        PlayerPrefs.SetString("IDCode", RegistrationManager.playerCode);
         PlayerPrefs.SetString("Gender", RegistrationManager.playerGender);
+        PlayerPrefs.SetString("Name", RegistrationManager.playerName);
+        SceneManager.LoadScene("Onboarding");
 
-        await docRef.SetAsync(log, SetOptions.MergeAll).ContinueWithOnMainThread(task =>
-                                    {
-                                        Debug.Log("Data updated to the log.");
-                                        SceneManager.LoadScene("Onboarding");
-                                    });
+        // app = Firebase.FirebaseApp.DefaultInstance;
+        // FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        // DocumentReference docRef;
+        // docRef = db.Collection("game_keys").Document(pCode.text);
+        // Dictionary<string, object> log = new Dictionary<string, object>{
+        //                     {"binded_device", SystemInfo.deviceUniqueIdentifier},
+        //                     {"activated", pCode.text=="05ada8"?false:true}
+        //                 };
+
+        // Debug.Log("Registering player");
+        // PlayerPrefs.SetInt("Life", 3);
+        // RegistrationManager.playerName = pName.GetComponent<Text>().text;
+        // RegistrationManager.playerCode = pCode.GetComponent<InputField>().text;
+        // PlayerPrefs.SetString("Name", RegistrationManager.playerName);
+        // PlayerPrefs.SetString("IDCode", RegistrationManager.playerCode);
+        // PlayerPrefs.SetString("Gender", RegistrationManager.playerGender);
+
+        // await docRef.SetAsync(log, SetOptions.MergeAll).ContinueWithOnMainThread(task =>
+        //                             {
+        //                                 Debug.Log("Data updated to the log.");
+        //                                 SceneManager.LoadScene("Onboarding");
+        //                             });
 
     }
 
-    public async void StoreProfile()
-    {
-        // Error checkup
-        bool error = false;
-        string errorMessages = "";
+    // public async void StoreProfile()
+    // {
+    //     // Error checkup
+    //     bool error = false;
+    //     string errorMessages = "";
 
 
-        if (SystemInfo.deviceType == DeviceType.Desktop)
-        {
-            app = Firebase.FirebaseApp.DefaultInstance;
-            FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
-            DocumentReference docRef;
+    //     if (SystemInfo.deviceType == DeviceType.Desktop)
+    //     {
+    //         app = Firebase.FirebaseApp.DefaultInstance;
+    //         FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+    //         DocumentReference docRef;
 
-            if (pCode.text.Length > 0)
-            {
-                docRef = db.Collection("game_keys").Document(pCode.text);
+    //         if (pCode.text.Length > 0)
+    //         {
+    //             docRef = db.Collection("game_keys").Document(pCode.text);
 
-                await docRef.GetSnapshotAsync().ContinueWith((task) =>
-                {
-                    var snapshot = task.Result;
-                    if (snapshot.Exists)
-                    {
-                        Debug.Log($"Key exists");
+    //             await docRef.GetSnapshotAsync().ContinueWith((task) =>
+    //             {
+    //                 var snapshot = task.Result;
+    //                 if (snapshot.Exists)
+    //                 {
+    //                     Debug.Log($"Key exists");
 
-                        Dictionary<string, object> gkey = snapshot.ToDictionary();
+    //                     Dictionary<string, object> gkey = snapshot.ToDictionary();
 
-                        Debug.Log($"Activated Status: {gkey["activated"]}");
+    //                     Debug.Log($"Activated Status: {gkey["activated"]}");
 
-                        if (bool.Parse(gkey["activated"].ToString()) == true)
-                        {
-                            error = true;
-                            errorMessages += "Your keycode already taken.\n";
-                        }
-                    }
-                    else
-                    {
-                        error = true;
-                        errorMessages += "Your keycode is invalid. Please review your entry.\n";
+    //                     if (bool.Parse(gkey["activated"].ToString()) == true)
+    //                     {
+    //                         error = true;
+    //                         errorMessages += "Your keycode already taken.\n";
+    //                     }
+    //                 }
+    //                 else
+    //                 {
+    //                     error = true;
+    //                     errorMessages += "Your keycode is invalid. Please review your entry.\n";
 
-                    }
-                });
+    //                 }
+    //             });
 
-                if (pName.GetComponent<Text>().text.Length < 2)
-                {
-                    error = true;
-                    errorMessages += "Invalid name. Please review your name.\n";
-                }
+    //             if (pName.GetComponent<Text>().text.Length < 2)
+    //             {
+    //                 error = true;
+    //                 errorMessages += "Invalid name. Please review your name.\n";
+    //             }
 
-                if (RegistrationManager.playerGender == null)
-                {
-                    error = true;
-                    errorMessages += "Missing gender selection.\n";
-                }
+    //             if (RegistrationManager.playerGender == null)
+    //             {
+    //                 error = true;
+    //                 errorMessages += "Missing gender selection.\n";
+    //             }
 
-                if (pCode.text.Length < 3)
-                {
-                    error = true;
-                    errorMessages += "Invalid code. Please check or ask your teacher.\n";
-                }
+    //             if (pCode.text.Length < 3)
+    //             {
+    //                 error = true;
+    //                 errorMessages += "Invalid code. Please check or ask your teacher.\n";
+    //             }
 
-                if (error)
-                {
-                    Debug.Log(errorMessages);
-                    ToggleError(errorMessages);
-                }
-                else
-                {
-                    RegisterPlayer();
-                }
-            }
-            else
-            {
-                error = true;
-                errorMessages += "Empty code. Please check or ask your teacher.\n";
-                ToggleError(errorMessages);
-            }
-        }
-        else
-        {
-            await Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(async (task) =>
-                {
-                    var dependencyStatus = task.Result;
-                    if (dependencyStatus == Firebase.DependencyStatus.Available)
-                    {
-                        app = Firebase.FirebaseApp.DefaultInstance;
-                        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
-                        DocumentReference docRef;
+    //             if (error)
+    //             {
+    //                 Debug.Log(errorMessages);
+    //                 ToggleError(errorMessages);
+    //             }
+    //             else
+    //             {
+    //                 RegisterPlayer();
+    //             }
+    //         }
+    //         else
+    //         {
+    //             error = true;
+    //             errorMessages += "Empty code. Please check or ask your teacher.\n";
+    //             ToggleError(errorMessages);
+    //         }
+    //     }
+    //     else
+    //     {
+    //         await Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(async (task) =>
+    //             {
+    //                 var dependencyStatus = task.Result;
+    //                 if (dependencyStatus == Firebase.DependencyStatus.Available)
+    //                 {
+    //                     app = Firebase.FirebaseApp.DefaultInstance;
+    //                     FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+    //                     DocumentReference docRef;
 
-                        if (pCode.text.Length > 0)
-                        {
-                            docRef = db.Collection("game_keys").Document(pCode.text);
+    //                     if (pCode.text.Length > 0)
+    //                     {
+    //                         docRef = db.Collection("game_keys").Document(pCode.text);
 
-                            await docRef.GetSnapshotAsync().ContinueWith((task) =>
-                            {
-                                var snapshot = task.Result;
-                                if (snapshot.Exists)
-                                {
-                                    Debug.Log($"Key exists");
+    //                         await docRef.GetSnapshotAsync().ContinueWith((task) =>
+    //                         {
+    //                             var snapshot = task.Result;
+    //                             if (snapshot.Exists)
+    //                             {
+    //                                 Debug.Log($"Key exists");
 
-                                    Dictionary<string, object> gkey = snapshot.ToDictionary();
+    //                                 Dictionary<string, object> gkey = snapshot.ToDictionary();
 
-                                    Debug.Log($"Activated Status: {gkey["activated"]}");
+    //                                 Debug.Log($"Activated Status: {gkey["activated"]}");
 
-                                    if (bool.Parse(gkey["activated"].ToString()) == true)
-                                    {
-                                        error = true;
-                                        errorMessages += "Your keycode already taken.\n";
-                                    }
-                                }
-                                else
-                                {
-                                    error = true;
-                                    errorMessages += "Your keycode is invalid. Please review your entry.\n";
+    //                                 if (bool.Parse(gkey["activated"].ToString()) == true)
+    //                                 {
+    //                                     error = true;
+    //                                     errorMessages += "Your keycode already taken.\n";
+    //                                 }
+    //                             }
+    //                             else
+    //                             {
+    //                                 error = true;
+    //                                 errorMessages += "Your keycode is invalid. Please review your entry.\n";
 
-                                }
-                            });
+    //                             }
+    //                         });
 
-                            if (pName.GetComponent<Text>().text.Length < 2)
-                            {
-                                error = true;
-                                errorMessages += "Invalid name. Please review your name.\n";
-                            }
+    //                         if (pName.GetComponent<Text>().text.Length < 2)
+    //                         {
+    //                             error = true;
+    //                             errorMessages += "Invalid name. Please review your name.\n";
+    //                         }
 
-                            if (RegistrationManager.playerGender == null)
-                            {
-                                error = true;
-                                errorMessages += "Missing gender selection.\n";
-                            }
+    //                         if (RegistrationManager.playerGender == null)
+    //                         {
+    //                             error = true;
+    //                             errorMessages += "Missing gender selection.\n";
+    //                         }
 
-                            if (pCode.text.Length < 3)
-                            {
-                                error = true;
-                                errorMessages += "Invalid code. Please check or ask your teacher.\n";
-                            }
+    //                         if (pCode.text.Length < 3)
+    //                         {
+    //                             error = true;
+    //                             errorMessages += "Invalid code. Please check or ask your teacher.\n";
+    //                         }
 
-                            if (error)
-                            {
-                                Debug.Log(errorMessages);
-                                ToggleError(errorMessages);
-                            }
-                            else
-                            {
-                                RegisterPlayer();
-                            }
-                        }
-                        else
-                        {
-                            error = true;
-                            errorMessages += "Empty code. Please check or ask your teacher.\n";
-                            ToggleError(errorMessages);
-                        }
-                    }
-                    else
-                    {
-                        error = true;
-                        errorMessages += "Unable to detect Google play dependencies.\n";
-                        ToggleError(errorMessages);
-                    }
-                });
-        }
-    }
+    //                         if (error)
+    //                         {
+    //                             Debug.Log(errorMessages);
+    //                             ToggleError(errorMessages);
+    //                         }
+    //                         else
+    //                         {
+    //                             RegisterPlayer();
+    //                         }
+    //                     }
+    //                     else
+    //                     {
+    //                         error = true;
+    //                         errorMessages += "Empty code. Please check or ask your teacher.\n";
+    //                         ToggleError(errorMessages);
+    //                     }
+    //                 }
+    //                 else
+    //                 {
+    //                     error = true;
+    //                     errorMessages += "Unable to detect Google play dependencies.\n";
+    //                     ToggleError(errorMessages);
+    //                 }
+    //             });
+    //     }
+    // }
     IEnumerator LateCall()
     {
         popUp.gameObject.SetActive(true);
