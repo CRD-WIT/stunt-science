eace6347acfcae02e1d1dd2f586b9d04572f4190
Mike <mhermogila@gmail.com>
2022-01-11 16:20:15 +0800

    CircularMotion Easy: done Merging

--- Assets/Scenes/LevelThreeMedium/Scripts/LevelData.cs
+++
@@ -1,8 +0,0 @@
-    public class LevelData{
-        int points;
-        bool locked;
-        public LevelData(int points, bool locked){
-            this.points = points;
-            this.locked = locked;
-        }
-    }
