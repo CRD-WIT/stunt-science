cc10bd7e4bc6f20325c5c6791f65b0d7a25401aa
Mike <mhermogila@gmail.com>
2022-01-13 16:26:28 +0800

    CircularMotionMedium: merged

--- Assets/Scripts/Mike/circular motion/medium/PlayerCM2.cs
+++ Assets/Scripts/Mike/circular motion/medium/PlayerCM2.cs
@@ -22,12 +22,17 @@ public class PlayerCM2 : MonoBehaviour
         isHanging,
         brake,
         isGrabbing,
+        climb,
         hangWalk,
         isFalling,
         toJump,
         jumpHang,
         isLanded,
-        walking;
+        running,
+        walking,
+        ropeGrab,
+        successGrab,
+        grab;
 
     // public AudioSource footstep;
     float currentpos;
@@ -65,9 +70,15 @@ public class PlayerCM2 : MonoBehaviour
         myAnimator.SetBool("toJump", toJump);
         myAnimator.SetBool("jumpHang", jumpHang);
         myAnimator.SetBool("landed", isLanded);
+        myAnimator.SetBool("climb", climb);
+        myAnimator.SetBool("running", running);
         myAnimator.SetBool("walking", walking);
+        myAnimator.SetBool("ropeGrab", ropeGrab);
+        myAnimator.SetBool("successGrab", successGrab);
+
+        if (climb)
+            myRigidbody.velocity = new Vector2(0, 1);
         if (posready == true)
-        {
             if (currentpos >= 0)
             {
                 moveSpeed = 0;
@@ -78,19 +89,14 @@ public class PlayerCM2 : MonoBehaviour
                 );
                 posready = false;
             }
-        }
         if (happy == true)
-        {
             StartCoroutine(happyOn());
-        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == ("stickmanspawn"))
-        {
             ragdollblow = true;
-        }
     }
 
     public void ragdollspawn()
@@ -109,23 +115,46 @@ public class PlayerCM2 : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        other.enabled = false;
         if (other.gameObject.tag == ("jumper"))
         {
-            other.enabled = false;
             jump();
         }
         else if (other.gameObject.tag == ("stickmanspawn"))
         {
             life.ReduceLife();
             ragDollTrigger = other;
-            other.enabled = false;
             ragdollspawn();
             RagdollV2.disableRagdoll = true;
             lost = false;
             standup = true;
         }
+        else if (other.gameObject.name == "jumper")
+        {
+            Debug.Log("etenr");
+
+            moveSpeed = 0;
+            running = false;
+            ropeGrab = true;
+            StartCoroutine(RopeGrab());
+        }
         else
             other.enabled = true;
+    }
+
+    IEnumerator RopeGrab()
+    {
+        yield return new WaitForSeconds(8 / 5);
+        successGrab = true;
+        yield return new WaitForSeconds(.5f);
+        successGrab = false;
+        grab = true;
+        ropeGrab = false;
+        yield return new WaitForEndOfFrame();
+        grab = false;
+        hangWalk = true;
+        yield return new WaitForSeconds(1.01f);
+        hangWalk = false;
     }
 
     public void ToggleTrigger()
