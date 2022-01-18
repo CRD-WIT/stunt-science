d9741978d3344f249c95276d41efca73c5c89068
biboy <junejaspe@gmail.com>
2022-01-18 11:48:28 +0800

    bugFixprojMed

--- Assets/Scripts/bibpyScript/ForcesHard/PrisonerManager.cs
+++ Assets/Scripts/bibpyScript/ForcesHard/PrisonerManager.cs
@@ -5,29 +5,53 @@ using UnityEngine;
 public class PrisonerManager : MonoBehaviour
 {
     public ForceHardManagerOne theManagerOne;
-    public bool ragdollReady;
+    public float moveSpeed;
+    public bool ragdollReady, runLeft, runRight;
+    public Rigidbody2D rb;
+    public Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        myAnimator.SetFloat("speed", moveSpeed);
+
+        if (runLeft)
+        {
+            moveSpeed = 4;
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+
+        if (runRight)
+        {
+            moveSpeed = 4;
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "box")
         {
-            if(ragdollReady)
+            if (ragdollReady)
             {
                 theManagerOne.ragdollSpawn();
                 ragdollReady = false;
             }
-            
+
         }
+    }
+    public IEnumerator startRun()
+    {
+        runLeft = true;
+        yield return new WaitForSeconds(5);
+        runLeft = false;
+        moveSpeed = 0;
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
     }
 }
